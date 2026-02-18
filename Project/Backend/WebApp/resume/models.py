654c31b3075f391abd92ba172c16bf10343672fb
Ganesh-Vankudoth <gvankadoth@gmail.com>
2026-02-18 11:55:55 +0530

    Day3:Added resume text extraction functionality

--- Project/Backend/WebApp/resume/models.py
+++ Project/Backend/WebApp/resume/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Resume(models.Model):
     file=models.FileField(upload_to='resumes/')
     uploaded_at=models.DateTimeField(auto_now_add=True)
-    
+    extracted_text=models.TextField(blank=True)
     def __str__(self):
         return self.file.name
--- Project/Backend/WebApp/resume/views.py
+++ Project/Backend/WebApp/resume/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .forms import ResumeUploadForm
 from .models import Resume
+from .utils import extract_text_from_pdf
 
 # Basic check view
 def display(request):
@@ -13,10 +14,14 @@ def upload_resume(request):
         # request.FILES is where the actual file data lives
         form = ResumeUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            resume=form.save()
+            file_path=resume.file.path
+            extracted_text=extract_text_from_pdf(file_path)
+            resume.extracted_text=extracted_text
+            resume.save()
             return JsonResponse({
                 "status": "success", 
-                "message": "Resume uploaded and saved to media/resumes/ folder!"
+                "message": "Resume uploaded and text extracted successfully!"
             })
     else:
         # Show an empty form for the GET request
